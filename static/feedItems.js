aab75013ce4db35de14bb3399a5c3eb48b8aaa4e
Guli <ignaciomorenopubul@gmail.com>
2020-02-09 16:59:04 +0100

    Change order of projects

--- static/feedItems.js
+++ static/feedItems.js
@@ -16,6 +16,92 @@ export default {
     },
     {
       id: 2,
+      type: 'NEWS',
+      poster: {
+        name: 'Comunidad de Madrid',
+        info: 'Comunidad de Madrid',
+        imgSrc:
+          'https://pbs.twimg.com/profile_images/1185678375647662080/-sj0LOtW_400x400.png'
+      },
+      content: `Madrid partners with Comgo to allow citizens track the social impact of their contributions. <a>@Comgo</a>. <a href="#">#blockchainforgood</a>`,
+      timestamp: '18:45',
+      imgSrc: 'https://comgo.io/_nuxt/img/school.6a9b6d6.png'
+    },
+    {
+      id: 3,
+      type: 'PRODUCT',
+      poster: {
+        name: 'Ecoalf',
+        info: 'Moda sostenible',
+        imgSrc: 'https://ecoalf.com/img/ecoalf-logo-1562319524.jpg'
+      },
+      content: `More than 10 million hectares of Australian land have been burned. Our planet is burning, we care and we want to help. ECOALF will donate 5% of its online sales for the reforestation and restoration.</a>`,
+      timestamp: '18:33',
+      product: 'Winter Coat Black',
+      price: '59.95',
+      unit: '€',
+      imgSrc:
+        'https://test.comgo.io/img/project/Project1581104090471/151538fotoecoalf3.jpg'
+    },
+    {
+      id: 4,
+      type: 'PRODUCT',
+      poster: {
+        name: 'AUARA',
+        info: 'Water to change the world',
+        imgSrc:
+          'https://cdn.shopify.com/s/files/1/0058/6498/0591/files/AUARA_PS_WEB_HOME_PRODUCTO_2020_2_600x.jpg?v=1580988796'
+      },
+      content: `1 out of 10 people do not have access to clean water. Once our founders saw that we founded a social entrepreneurship that donates 100% of the benefits to projects that ensure the most underprivileged people have access to clean water. <a>@Comgo</a>. <a href="#">#watertochangetheworld</a>
+        <a href="#">#FundacionCajaNavarra</a>`,
+      timestamp: '19:50',
+      location: 'Spain',
+      product: '6x Water Bottle Pack',
+      price: '3.49',
+      unit: '€',
+      imgSrc:
+        'https://test.comgo.io/img/project/Project1581100531630/108556AUARA_PS_WEB_NOSOTROS_FOTO_descargas_600x.jpg'
+    },
+    {
+      id: 5,
+      type: 'EVENT',
+      poster: {
+        name: 'Comgo',
+        info: 'The Common Good Chain',
+        imgSrc: 'https://comgo.io/_nuxt/img/logo.897afa0.png'
+      },
+      content: `Comgo organizes a training on Blockchain for Good in Madrid. Join us! <a>@comunidaddemadrid</a>. <a href="#">#blockchaintraining</a>`,
+      timestamp: '19:50',
+      location: 'Madrid',
+      when: 'March 31st, 2020',
+      imgSrc:
+        'https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      id: 6,
+      type: 'UPDATE',
+      poster: {
+        name: 'Cáritas',
+        info: 'Cáritas España',
+        imgSrc:
+          'https://caritas-web.s3.amazonaws.com/main-files/uploads/2018/12/logo_2x_caritas.png'
+      },
+      content: `Cáritas reaches 100 backers for their project supporting homeless centers in Madrid. <a>@caritas</a>. <a>#homeless</a> 
+        <a href="#">#Madrid</a>`,
+      timestamp: '19:50',
+      location: 'Madrid',
+      deadline: '30',
+      backers: '100',
+      goal: '15k',
+      goalNum: 15000,
+      covered: '7.5k',
+      coveredNum: 7500,
+      left: '6.9k',
+      imgSrc:
+        'https://caritas-web.s3.amazonaws.com/main-files/uploads/2017/11/320x480_destacado_accionsocial.jpg'
+    },
+    {
+      id: 7,
       type: 'PROJECT',
       projectId: 'Project1581156418542',
       poster: {
@@ -37,92 +123,6 @@ export default {
       left: '31k',
       imgSrc:
         'https://www.accioncontraelhambre.org/sites/default/files/styles/story_image/public/images/stories/cabecera_SA.jpg?itok=Z-GTQqUm'
-    },
-    {
-      id: 3,
-      type: 'NEWS',
-      poster: {
-        name: 'Comunidad de Madrid',
-        info: 'Comunidad de Madrid',
-        imgSrc:
-          'https://pbs.twimg.com/profile_images/1185678375647662080/-sj0LOtW_400x400.png'
-      },
-      content: `Madrid partners with Comgo to allow citizens track the social impact of their contributions. <a>@Comgo</a>. <a href="#">#blockchainforgood</a>`,
-      timestamp: '18:45',
-      imgSrc: 'https://comgo.io/_nuxt/img/school.6a9b6d6.png'
-    },
-    {
-      id: 4,
-      type: 'PRODUCT',
-      poster: {
-        name: 'Ecoalf',
-        info: 'Moda sostenible',
-        imgSrc: 'https://ecoalf.com/img/ecoalf-logo-1562319524.jpg'
-      },
-      content: `More than 10 million hectares of Australian land have been burned. Our planet is burning, we care and we want to help. ECOALF will donate 5% of its online sales for the reforestation and restoration.</a>`,
-      timestamp: '18:33',
-      product: 'Winter Coat Black',
-      price: '59.95',
-      unit: '€',
-      imgSrc:
-        'https://test.comgo.io/img/project/Project1581104090471/151538fotoecoalf3.jpg'
-    },
-    {
-      id: 5,
-      type: 'PRODUCT',
-      poster: {
-        name: 'AUARA',
-        info: 'Water to change the world',
-        imgSrc:
-          'https://cdn.shopify.com/s/files/1/0058/6498/0591/files/AUARA_PS_WEB_HOME_PRODUCTO_2020_2_600x.jpg?v=1580988796'
-      },
-      content: `1 out of 10 people do not have access to clean water. Once our founders saw that we founded a social entrepreneurship that donates 100% of the benefits to projects that ensure the most underprivileged people have access to clean water. <a>@Comgo</a>. <a href="#">#watertochangetheworld</a>
-        <a href="#">#FundacionCajaNavarra</a>`,
-      timestamp: '19:50',
-      location: 'Spain',
-      product: '6x Water Bottle Pack',
-      price: '3.49',
-      unit: '€',
-      imgSrc:
-        'https://test.comgo.io/img/project/Project1581100531630/108556AUARA_PS_WEB_NOSOTROS_FOTO_descargas_600x.jpg'
-    },
-    {
-      id: 6,
-      type: 'EVENT',
-      poster: {
-        name: 'Comgo',
-        info: 'The Common Good Chain',
-        imgSrc: 'https://comgo.io/_nuxt/img/logo.897afa0.png'
-      },
-      content: `Comgo organizes a training on Blockchain for Good in Madrid. Join us! <a>@comunidaddemadrid</a>. <a href="#">#blockchaintraining</a>`,
-      timestamp: '19:50',
-      location: 'Madrid',
-      when: 'March 31st, 2020',
-      imgSrc:
-        'https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      id: 8,
-      type: 'UPDATE',
-      poster: {
-        name: 'Cáritas',
-        info: 'Cáritas España',
-        imgSrc:
-          'https://caritas-web.s3.amazonaws.com/main-files/uploads/2018/12/logo_2x_caritas.png'
-      },
-      content: `Cáritas reaches 100 backers for their project supporting homeless centers in Madrid. <a>@caritas</a>. <a>#homeless</a> 
-        <a href="#">#Madrid</a>`,
-      timestamp: '19:50',
-      location: 'Madrid',
-      deadline: '30',
-      backers: '100',
-      goal: '15k',
-      goalNum: 15000,
-      covered: '7.5k',
-      coveredNum: 7500,
-      left: '6.9k',
-      imgSrc:
-        'https://caritas-web.s3.amazonaws.com/main-files/uploads/2017/11/320x480_destacado_accionsocial.jpg'
     }
   ]
 }
